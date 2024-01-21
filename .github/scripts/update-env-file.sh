e082d7eddea59df25b50dc70c2daa45a5890e325
Dhfernando <hdilshan.fernando11@gmail.com>
2024-01-21 12:38:02 +0530

    iniy

--- .github/scripts/update-env-file.sh
+++ .github/scripts/update-env-file.sh
@@ -6,9 +6,9 @@ content=$(<"$templateFile")
 
 # Replace placeholders with specific test values
 # Replace placeholders with actual values from GitHub Secrets
-content=${content//COMMAND_SERVER_PORT=/${{ vars.COMMAND_SERVER_PORT }}}
-content=${content//QUERY_SERVER_PORT=/${{ vars.QUERY_SERVER_PORT }}}
-content=${content//WORKFLOW_ENGINE_PORT=/${{ vars.WORKFLOW_ENGINE_PORT }}}
+content=${content//COMMAND_SERVER_PORT=/$COMMAND_SERVER_PORT}
+content=${content//QUERY_SERVER_PORT=/$QUERY_SERVER_PORT}
+content=${content//WORKFLOW_ENGINE_PORT=/$WORKFLOW_ENGINE_PORT}
 
 # Save the modified content to a new file
 envFile=".env"
